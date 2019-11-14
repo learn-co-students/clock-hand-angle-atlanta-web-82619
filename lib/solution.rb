b896b97df4e51142f13ebc574f0eb525d81aea41
Justina Kuchta <44618164+jwkuchta@users.noreply.github.com>
2019-11-14 10:00:17 -0500

    Update solution.rb

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,18 +1,13 @@
 def clock_angle(time)
-    # code your solution here
-    h = time.split(":")[0].to_i
-    m = time.split(":")[1].to_i
-    angle = nil
+    h = time.split(":")[0].to_f
+    m = time.split(":")[1].to_f
 
     if h == 12
         h = 0
     end
-    if m == 60
-        m = 0
-    end
-      
-    hour_angle = 0.5 * (h * 60 + m) 
+       
     minute_angle = 6 * m  
+    hour_angle =  30 * h + m/60 * 30
     angle = (hour_angle - minute_angle).abs
     angle = [360 - angle, angle].min
 end
